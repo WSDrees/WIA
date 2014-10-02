fbc526133f990dc5abca5aa6cb27744e223a261b
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 15:14:15 -0400

    Class work
    images

--- Day2-Student-Files/HTML5 Adv/js/Images.js
+++ Day2-Student-Files/HTML5 Adv/js/Images.js
@@ -4,9 +4,18 @@ window.onload = function() {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
 
-				
-				
-     						
+        //Create a variable that holds the image
+        var srcImg = document.getElementById('img1');
+
+        //Draw the image directly onto the canvas at the top left, normal size
+        //ctx.drawImage(srcImg,0,0);
+
+        //Draw and scale the image
+        //ctx.drawImage(srcImg,50,50,350,150);
+
+        //Draw a portion SLICE of the source image
+        ctx.drawImage(srcImg,350,200,125,100,50,50,125,100);
+
 		}
 	}
-}
+};
