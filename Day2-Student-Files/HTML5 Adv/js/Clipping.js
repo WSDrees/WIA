7d45fc56788a1bf22d5eddb4349914a5af102806
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 15:45:00 -0400

    class updates
    Clipping

--- Day2-Student-Files/HTML5 Adv/js/Clipping.js
+++ Day2-Student-Files/HTML5 Adv/js/Clipping.js
@@ -1,11 +1,37 @@
 window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
-	var ctx = theCanvas.getContext("2d");
-	if (ctx) {
-  		 
-		 
-		 
-		}
-	}
-}
+    var theCanvas = document.getElementById('Canvas1');
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        if (ctx) {
+
+            //Draw an image
+
+            //Get our image into a variable
+            var srcImg = document.getElementById('img1');
+
+
+            //Create a circle clipping path
+//            ctx.beginPath();
+//            ctx.arc(ctx.canvas.width/2, ctx.canvas.height/2, 150, 0, 2*Math.PI);
+//            ctx.clip();
+
+
+
+            //Create a weird path to clip
+
+            ctx.beginPath();
+            ctx.moveTo(105,200);
+            ctx.lineTo(250,25);
+            ctx.lineTo(525,50);
+            ctx.lineTo(475,285);
+            ctx.closePath();
+
+            ctx.clip();
+
+            ctx.drawImage(srcImg,0,0);
+
+
+
+        }
+    }
+};
