a7032d609a14a39ecbee7c12cabaeeabdac366c3
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 16:07:58 -0400

    class updates
    Scale

--- Day2-Student-Files/HTML5 Adv/js/Scale.js
+++ Day2-Student-Files/HTML5 Adv/js/Scale.js
@@ -1,11 +1,25 @@
 window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
-	var ctx = theCanvas.getContext("2d");
-	if (ctx) {
-		
-			
-						
-		}
-	}
-}
+    var theCanvas = document.getElementById('Canvas1');
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        if (ctx) {
+
+            //Draw a rectangle
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(0,0,100,50);
+
+            //Set scale factor
+            ctx.scale(2,2);
+
+            ctx.fillStyle = 'red';
+            ctx.fillRect(0,50,100,50);
+
+            ctx.scale(.5,.5);
+
+            ctx.fillStyle = 'green';
+            ctx.fillRect(0,225,100,50);
+
+
+        }
+    }
+};
