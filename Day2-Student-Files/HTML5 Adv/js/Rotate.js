60b636068271cd1e745f557b13c4799aa762f2b7
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 16:25:14 -0400

    updates

--- Day2-Student-Files/HTML5 Adv/js/Rotate.js
+++ Day2-Student-Files/HTML5 Adv/js/Rotate.js
@@ -1,11 +1,40 @@
 window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
-	var ctx = theCanvas.getContext("2d");
-	if (ctx) {
-				
-				
-						
-		}
-	}
-}
+    var theCanvas = document.getElementById('Canvas1');
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        if (ctx) {
+
+
+            //Save the context
+            ctx.save();
+
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(150,30,100,50);
+
+            //Rotate around the origin
+            ctx.rotate((Math.PI/180)*45);       //Math to convert to degrees from radians
+
+            //Draw a 2nd rectangle
+            ctx.fillStyle = 'red';
+            ctx.fillRect(150,30,100,50);
+
+            ctx.restore();
+
+            //Translate the origin
+            ctx.translate(200,55);
+
+            ctx.rotate((Math.PI/180)*45);
+
+            //Draw a third rectangle
+            ctx.fillStyle = 'green';
+            ctx.fillRect(-75,-15,100,50);
+
+
+
+
+
+
+
+        }
+    }
+};
--- Day2-Student-Files/HTML5 Adv/js/Scale.js
+++ Day2-Student-Files/HTML5 Adv/js/Scale.js
@@ -4,12 +4,18 @@ window.onload = function() {
         var ctx = theCanvas.getContext("2d");
         if (ctx) {
 
+            //Save context
+            ctx.save();
+
             //Draw a rectangle
             ctx.fillStyle = 'blue';
             ctx.fillRect(0,0,100,50);
 
             //Set scale factor
             ctx.scale(2,2);
+
+            //Restore content
+            ctx.restore();
 
             ctx.fillStyle = 'red';
             ctx.fillRect(0,50,100,50);
