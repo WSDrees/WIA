31b5eb69027ef1662ebe8faedbe7396ab37f1997
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 15:21:41 -0400

    class updates
    video and images

--- Day2-Student-Files/HTML5 Adv/js/Images.js
+++ Day2-Student-Files/HTML5 Adv/js/Images.js
@@ -14,7 +14,23 @@ window.onload = function() {
         //ctx.drawImage(srcImg,50,50,350,150);
 
         //Draw a portion SLICE of the source image
-        ctx.drawImage(srcImg,350,200,125,100,50,50,125,100);
+        //ctx.drawImage(srcImg,350,200,125,100,50,50,125,100);
+
+        //Draw the video onto the canvas
+        //Setup an interval function
+        //Grab each image as the video plays and then PAINT it on the canvas
+        //Create a variable to hold the video
+        var srcVid = document.getElementById('vid1');
+
+        //Start the video
+        srcVid.play();
+
+        //Setup the interval
+        setInterval(function(){
+
+            ctx.drawImage(srcVid,0,0);
+
+        }, 16);
 
 		}
 	}
