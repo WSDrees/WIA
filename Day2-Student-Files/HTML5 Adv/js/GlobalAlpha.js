50495b441ad84f7e45cb1192ad6f1318d9cfb764
WSDrees <SteveDrees@fullsail.edu>
2014-10-02 16:35:49 -0400

    class updates
    GlobalAlpha

--- Day2-Student-Files/HTML5 Adv/js/GlobalAlpha.js
+++ Day2-Student-Files/HTML5 Adv/js/GlobalAlpha.js
@@ -1,27 +1,40 @@
 window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
-	var ctx = theCanvas.getContext("2d");
-	if (ctx) {
-				
-				
-				// Create an array of rectangles
-				
-				var rects = [
-					{x:20, y:20, width:75, height:75 },
-					{x:40, y:50, width:75, height:75 },
-					{x:150, y:20, width:75, height:75 },
-					{x:170, y:50, width:75, height:75 },
-					{x:280, y:20, width:75, height:75 },
-					{x:300, y:50, width:75, height:75 },
-					{x:410, y:20, width:75, height:75 },
-					{x:430, y:50, width:75, height:75 },
-				]
-				
-				
-				
-				
+    var theCanvas = document.getElementById('Canvas1');
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        if (ctx) {
 
-		}
-	}
-}
+
+            // Create an array of rectangles
+
+            var rects = [
+                {x:20, y:20, width:75, height:75 },
+                {x:40, y:50, width:75, height:75 },
+                {x:150, y:20, width:75, height:75 },
+                {x:170, y:50, width:75, height:75 },
+                {x:280, y:20, width:75, height:75 },
+                {x:300, y:50, width:75, height:75 },
+                {x:410, y:20, width:75, height:75 },
+                {x:430, y:50, width:75, height:75 }
+            ];
+
+
+            //Draw the array of rectangles and change the Global Alpha
+            for(var i=0; i<rects.length; i+=2){
+                //Setup Styles
+                ctx.fillStyle = 'blue';
+                ctx.fillRect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
+
+                ctx.fillStyle = 'red';
+                ctx.fillRect(rects[i+1].x, rects[i+1].y, rects[i+1].width, rects[i+1].height);
+
+                //Change the global alpha each time around
+                ctx.globalAlpha -=.3;
+
+            }
+
+
+
+        }
+    }
+};
