625496d35b5b408a77aa2e255f870e3e05f4ac40
WSDrees <SteveDrees@fullsail.edu>
2014-10-04 16:51:18 -0400

    animation with jQuery

---
+++ Day3-StudentFiles_ecc40b27-16c3-4e81-8e0c-9d442c7e645d/4-jQuery/js/Animate.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    $('#right').click(function(){
+
+        $('#theDiv').animate({width:'500px'},1000);
+
+    });
+
+    $('#text').click(function(){
+
+        $('#theDiv').animate({fontSize:'24pt'},1000);
+
+    });
+
+    $('#toggle').click(function(){
+
+        $('#theDiv').animate({left:"500"},1000,'swing');
+
+    });
+
+    $('#multiple').click(function() {
+
+       // $('#theDiv').animate({width:'500px', left:'500', fontSize:'24pt'},1000,'swing');
+
+        //Que of mini animations
+        $('#theDiv').animate({width:'500px'},1000,'swing');
+        $('#theDiv').animate({fontSize:'24pt'},1000,'swing');
+        $('#theDiv').animate({left:'500'},1000,'swing');
+    });
+
+    $('#reset').click(function(){
+
+        $('#theDiv').animate({width:'250px', left:'0', fontSize:'16pt'},1000,'swing');
+    })
+
+
+});
