620b045f90afe5fd8b40d5e6073d76316f44e2ed
WSDrees <SteveDrees@fullsail.edu>
2014-10-09 16:01:01 -0400

    Rectangle
    finished

--- Day5-StudentFiles_c478b450-bee3-4748-b35a-3c783135a6bb/CreateJS/js/Rectangle.js
+++ Day5-StudentFiles_c478b450-bee3-4748-b35a-3c783135a6bb/CreateJS/js/Rectangle.js
@@ -1,5 +1,31 @@
 window.onload = function() {
 
+    var stage = new createjs.Stage('canvas1');
+
+    //Create a new shape
+    var shape = new createjs.Shape();
+
+    //Set stroke style
+    shape.graphics.beginStroke('purple');
+    shape.graphics.setStrokeStyle(10,0,0);
+
+    //Draw a normal rectangle
+    shape.graphics.rect(50,50,300,300);
+
+    //Rounded Rectangle
+    shape.graphics.beginStroke('blue');
+    shape.graphics.drawRoundRect(75,75,250,250,30);
+
+    //Complex rounded rectangle
+    shape.graphics.beginStroke('red');
+    shape.graphics.drawRoundRectComplex(100,100,200,200,50,0,50,0);
+
+    //Add to the stage
+    stage.addChild(shape);
+
+    //Update our stage
+    stage.update();
+
 
 
 
