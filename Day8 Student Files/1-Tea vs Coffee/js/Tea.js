a603752c67810716aa489d2dc1b6036ac48e6048
WSDrees <SteveDrees@fullsail.edu>
2014-10-16 15:21:47 -0400

    Tea updates

--- Day8 Student Files/1-Tea vs Coffee/js/Tea.js
+++ Day8 Student Files/1-Tea vs Coffee/js/Tea.js
@@ -1,6 +1,19 @@
-$(function() { 
+$(function() {
 
+    //Hide the labels and then animate them in
+    $('#coffeeNumber').hide();
+    $('#teaNumber').hide();
 
+    //Make the animation button work
+    $('#moveIt').click(function(){
+
+        //Animate the width of each of the bars
+        $('#coffee').animate({width:'567px',height:'50px'},1000);
+        $('#coffeeNumber').slideDown(1000);
+
+        $('#tea').animate({width:'300px',height:'50px'},1000);
+        $('#teaNumber').slideDown(1000);
+    })
 		
 		
 });
