bf964c103001793cb9d11af01106788a4a17debe
WSDrees <SteveDrees@fullsail.edu>
2014-10-23 16:33:04 -0400

    Updates

--- drees_steve_WIA_Final/js/main.js
+++ drees_steve_WIA_Final/js/main.js
@@ -29,7 +29,20 @@ window.onload = function() {
 };
 
 
-
+//Hide the labels and then animate them in
+//$('#coffeeNumber').hide();
+//$('#teaNumber').hide();
+//
+////Make the animation button work
+//$('#moveIt').click(function(){
+//
+//    //Animate the width of each of the bars
+//    $('#coffee').animate({width:'567px',height:'50px'},1000);
+//    $('#coffeeNumber').slideDown(1000);
+//
+//    $('#tea').animate({width:'300px',height:'50px'},1000);
+//    $('#teaNumber').slideDown(1000);
+//})
 
 $(function(){
 
@@ -38,7 +51,9 @@ $(function(){
  $('#research').click(function(){
 
         $('.gender').show('normal');
- })
+ });
+
+    $('')
 
 });
 
